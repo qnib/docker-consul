f223e40def924f6985e84647c39860797bb9135f
Christian Kniep <christian@qnib.org>
2015-01-11 11:49:18 +0100

    adjust consul config

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,7 @@ FROM qnib/supervisor
 MAINTAINER "Christian Kniep <christian@qnib.org>"
 
 RUN yum install -y qnib-consul
+EXPOSE 8300
+
 ADD etc/supervisord.d/consul.ini /etc/supervisord.d/
+
---
+++ opt/qnib/bin/start_consul.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PIDFILE=/var/run/consul.pid
+
+# if consul in env, join
+
+## Check if eth0 already exists
+ADDR=eth1
+ip addr show ${ADDR} > /dev/null
+EC=$?
+if [ ${EC} -eq 1 ];then
+    echo "## Wait for pipework to attach device 'eth1'"
+    pipework --wait
+fi
+
+trap "{ kill $(cat ${PIDFILE}) }" SIGINT SIGTERM
+
+/opt/consul/consul agent -pid-file=${PIDFILE} -server -data-dir /var/consul/ -config-dir=/etc/consul.d/ \
+                         -bootstrap-expect 1 -ui-dir /opt/consul/dist/ -client=0.0.0.0
+
