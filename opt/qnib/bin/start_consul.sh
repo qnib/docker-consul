9c980ae4c92ac7860d60fa2483793f61d2cd903f
Christian Kniep <christian@qnib.org>
2015-01-11 15:04:12 +0100

    config file screw things up... :( back to arguments w/o dns-port changed

--- opt/qnib/bin/start_consul.sh
+++ opt/qnib/bin/start_consul.sh
@@ -9,11 +9,11 @@ if [ "X${CONSUL_PORT_8300_TCP_ADDR}" != "X" ];then
 fi
 
 ## Check if eth0 already exists
-ADDR=eth1
+ADDR=eth0
 ip addr show ${ADDR} > /dev/null
 EC=$?
 if [ ${EC} -eq 1 ];then
-    echo "## Wait for pipework to attach device 'eth1'"
+    echo "## Wait for pipework to attach device 'eth0'"
     pipework --wait
 fi
 
@@ -23,9 +23,12 @@ mkdir -p /etc/consul.d
 mkdir -p /var/consul/
 
 if [ "X${JOIN}" != "X" ];then
-    /usr/bin/consul agent -pid-file=${PIDFILE} -config-file=/etc/consul_client.json -config-dir=/etc/consul.d/ ${JOIN} &
+    # join someone - therefore a client
+    /usr/bin/consul agent -pid-file=${PIDFILE} -server -data-dir /var/consul/ -config-dir=/etc/consul.d/ ${JOIN} &
 else
-    /usr/bin/consul agent -pid-file=${PIDFILE} -config-file=/etc/consul_server.json -config-dir=/etc/consul.d/ &
+    # server mode
+    /usr/bin/consul agent -pid-file=${PIDFILE} -server -data-dir /var/consul/ -config-dir=/etc/consul.d/ \
+    -bootstrap-expect 1 -ui-dir /opt/consul-web-ui/ -client=0.0.0.0 &
 fi
 
 while [ -f ${PIDFILE} ];do
