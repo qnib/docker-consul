95dd03157d33c60726d7860a4b0ae73500d21196
Christian Kniep <christian.kniep@locafox.de>
2015-01-27 09:07:28 +0100

    DC_NAME customizable through environment

--- opt/qnib/bin/start_consul.sh
+++ opt/qnib/bin/start_consul.sh
@@ -22,6 +22,9 @@ done
 if [ "X${LINKED_SERVER}" == "X$(ip -o -4 address show eth0|awk '{print $4}'|awk -F\/ '{print $1}')" ];then
     LINKED_SERVER=0
 fi
+if [ "X${DC_NAME}" != "X" ];then
+    sed -i -e "s#\"datacenter\":.*#\"datacenter\": \"${DC_NAME}\",#" /etc/consul.json
+fi
 if [ "X${LINKED_SERVER}" != "X0" ];then
     sed -i -e "s#\"server\":.*#\"server\": false,#" /etc/consul.json
     sed -i -e "s#\"bootstrap\":.*#\"bootstrap\": false,#" /etc/consul.json
