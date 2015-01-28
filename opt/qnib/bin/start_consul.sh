bd0eec7d3212d238ef8c069b0209321edfd48a22
Christian Kniep <christian.kniep@locafox.de>
2015-01-28 09:58:33 +0100

    enable addvertise_address to be specified

--- opt/qnib/bin/start_consul.sh
+++ opt/qnib/bin/start_consul.sh
@@ -22,6 +22,20 @@ done
 if [ "X${LINKED_SERVER}" == "X$(ip -o -4 address show eth0|awk '{print $4}'|awk -F\/ '{print $1}')" ];then
     LINKED_SERVER=0
 fi
+## Check if eth0 already exists
+IPv4_RAW=$(ip -o -4 addr show ${ADDR})
+EC=$?
+if [ ${EC} -eq 1 ];then
+    echo "## Wait for pipework to attach device 'eth0'"
+    pipework --wait
+    IPv4_RAW=$(ip -o -4 addr show ${ADDR})
+fi
+IPv4=$(echo ${IPv4_RAW}|egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+")
+if [ "X${ADDV_ADDR}" != "X" ];then
+    sed -i -e "s#\"advertise_addr\":.*#\"advertise_addr\": \"${ADDV_ADDR}\",#" /etc/consul.json
+else
+    sed -i -e "s#\"advertise_addr\":.*#\"advertise_addr\": \"${IPv4}\",#" /etc/consul.json
+fi
 if [ "X${DC_NAME}" != "X" ];then
     sed -i -e "s#\"datacenter\":.*#\"datacenter\": \"${DC_NAME}\",#" /etc/consul.json
 fi
@@ -30,14 +44,6 @@ if [ "X${LINKED_SERVER}" != "X0" ];then
     sed -i -e "s#\"bootstrap\":.*#\"bootstrap\": false,#" /etc/consul.json
     sed -i -e "s#\"start_join\":.*#\"start_join\": [\"${LINKED_SERVER}\"],#" /etc/consul.json
 fi
-## Check if eth0 already exists
-ip addr show ${ADDR} > /dev/null
-EC=$?
-if [ ${EC} -eq 1 ];then
-    echo "## Wait for pipework to attach device 'eth0'"
-    pipework --wait
-fi
-
 
 mkdir -p /etc/consul.d
 mkdir -p /var/consul/
