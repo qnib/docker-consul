c3ef13723ae371560e699219dfe6778ca9cc0c1a
Christian Kniep <christian.kniep@locafox.de>
2015-01-29 10:36:41 +0100

    dedicatd variable to switch server on/off and more documentation

--- README.md
+++ README.md
@@ -10,3 +10,36 @@ The Image provides (not yet) the WebUI and exposes the PORT that can be fetched 
 ```
 $ docker run -d --name consul -h consul qnib/consul
 ```
+
+The default configuration looks like this:
+```
+{
+    "bootstrap": true,                  # fresh start
+    "server": true,                     # enable server mode -> RUN_SERVER=false
+    "datacenter": "qnib",               # set DC name -> DC_NAME=dc1
+    "data_dir": "/var/consul",
+    "log_level": "INFO",
+    "enable_syslog": false,
+    "ui_dir": "/opt/consul-web-ui/",
+    "client_addr": "0.0.0.0",
+    "start_join": [],                   # CONSUL_SERVER=<IP> / --links <CONTAINER>:consul
+    "advertise_addr": "",               # ADDV_ADDR=<IP>
+    "recursor": "8.8.8.8",
+    "ports": {
+        "dns": 53
+    }
+
+}
+```
+### Control consul agent
+
+The behavior of the agent is controlable through environment variables or linked containers.
+
+- **Join server**
+
+To join a consul server running within a dedicated container the container has to be linked:```--link CONTAINER_NAME:consul```
+This should provide the environment variable ```PORT_8500_TCP_ADDR``` (exposed port 8500). 
+
+A plain way of joining a server running on a specific IP address is achieved by providing the address via ```CONSUL_SERVER```.
+
+- 
--- opt/qnib/bin/start_consul.sh
+++ opt/qnib/bin/start_consul.sh
@@ -3,6 +3,7 @@
 PIDFILE=/var/run/consul.pid
 CONSUL_BIN=/usr/local/bin/consul
 ADDR=eth0
+RUN_SERVER=${RUN_SERVER:auto}
 
 if [ ! -d ${CONSUL_BIN} ];then
    CONSUL_BIN=/usr/bin/consul
@@ -19,48 +20,57 @@ for env_line in $(env);do
         break
     fi
 done
-if [ "X${LINKED_SERVER}" == "X$(ip -o -4 address show eth0|awk '{print $4}'|awk -F\/ '{print $1}')" ];then
-    LINKED_SERVER=0
-fi
-## Check if eth0 already exists
-IPv4_RAW=$(ip -o -4 addr show ${ADDR})
-EC=$?
-if [ ${EC} -eq 1 ];then
-    echo "## Wait for pipework to attach device 'eth0'"
-    pipework --wait
-    IPv4_RAW=$(ip -o -4 addr show ${ADDR})
-fi
-IPv4=$(echo ${IPv4_RAW}|egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+")
-if [ "X${ADDV_ADDR}" != "X" ];then
-    if [ "X${ADDV_ADDR}" == "XSERVER" ];then
-        ADDV_ADDR=$(cat /host_info/ip_eth0)
-    fi
-    sed -i -e "s#\"advertise_addr\":.*#\"advertise_addr\": \"${ADDV_ADDR}\",#" /etc/consul.json
+if [ "X${NO_CONSUL}" != "X" ];then
+    echo "Do not start any docker server"
+    touch ${PIDFILE}
 else
-    sed -i -e "s#\"advertise_addr\":.*#\"advertise_addr\": \"${IPv4}\",#" /etc/consul.json
-fi
-if [ "X${DC_NAME}" != "X" ];then
-    sed -i -e "s#\"datacenter\":.*#\"datacenter\": \"${DC_NAME}\",#" /etc/consul.json
-fi
-if [ "X${LINKED_SERVER}" != "X0" ];then
-    sed -i -e "s#\"server\":.*#\"server\": false,#" /etc/consul.json
-    sed -i -e "s#\"bootstrap\":.*#\"bootstrap\": false,#" /etc/consul.json
-    sed -i -e "s#\"start_join\":.*#\"start_join\": [\"${LINKED_SERVER}\"],#" /etc/consul.json
-fi
+    if [ "X${LINKED_SERVER}" == "X$(ip -o -4 address show eth0|awk '{print $4}'|awk -F\/ '{print $1}')" ];then
+        LINKED_SERVER=0
+    fi
+    ## Check if eth0 already exists
+    IPv4_RAW=$(ip -o -4 addr show ${ADDR})
+    EC=$?
+    if [ ${EC} -eq 1 ];then
+        echo "## Wait for pipework to attach device 'eth0'"
+        pipework --wait
+        IPv4_RAW=$(ip -o -4 addr show ${ADDR})
+    fi
+    IPv4=$(echo ${IPv4_RAW}|egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+")
+    if [ "X${ADDV_ADDR}" != "X" ];then
+        if [ "X${ADDV_ADDR}" == "XSERVER" ];then
+            ADDV_ADDR=$(cat /host_info/ip_eth0)
+        fi
+        sed -i -e "s#\"advertise_addr\":.*#\"advertise_addr\": \"${ADDV_ADDR}\",#" /etc/consul.json
+    else
+        sed -i -e "s#\"advertise_addr\":.*#\"advertise_addr\": \"${IPv4}\",#" /etc/consul.json
+    fi
+    if [ "X${DC_NAME}" != "X" ];then
+        sed -i -e "s#\"datacenter\":.*#\"datacenter\": \"${DC_NAME}\",#" /etc/consul.json
+    fi
+    if [ "X${LINKED_SERVER}" != "X0" ];then
+        RUN_SERVER=false
+        sed -i -e "s#\"start_join\":.*#\"start_join\": [\"${LINKED_SERVER}\"],#" /etc/consul.json
+    fi
+    
+    ## If we should join another server
+    JOIN_WAN=""
+    if [ "X${WAN_SERVER}" != "X" ];then
+        JOIN_WAN="-join-wan=${WAN_SERVER}"
+    fi
+    
+    if [ "X${RUN_SERVER}" == "Xfalse" ];then
+        sed -i -e "s#\"server\":.*#\"server\": false,#" /etc/consul.json
+        sed -i -e "s#\"bootstrap\":.*#\"bootstrap\": false,#" /etc/consul.json
+    fi
 
-## If we should join another server
-JOIN_WAN=""
-if [ "X${WAN_SERVER}" != "X" ];then
-    JOIN_WAN="-join-wan=${WAN_SERVER}"
+    mkdir -p /etc/consul.d
+    mkdir -p /var/consul/
+    ${CONSUL_BIN} agent -pid-file=${PIDFILE} -config-file=/etc/consul.json -config-dir=/etc/consul.d ${JOIN_WAN} &
 fi
-
-mkdir -p /etc/consul.d
-mkdir -p /var/consul/
-${CONSUL_BIN} agent -pid-file=${PIDFILE} -config-file=/etc/consul.json -config-dir=/etc/consul.d ${JOIN_WAN} &
 
 sleep 1
 
-trap "kill -9 $(cat ${PIDFILE})" SIGINT SIGTERM 15 9 10
+trap "kill -9 $(cat ${PIDFILE});rm -f ${PIDFILE}" SIGINT SIGTERM 15 9 10
 
 while [ -f ${PIDFILE} ];do
     sleep 1
