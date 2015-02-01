4dd2fbb21855a52e8ce1c2b6a3e817098018fdaa
Christian Kniep <christian@qnib.org>
2015-02-01 12:07:53 +0100

    typo in link syslog-config

--- opt/qnib/bin/start_syslog-ng.sh
+++ opt/qnib/bin/start_syslog-ng.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "X${ENABLE_SYSLOG}" == "Xtrue" ];then
-   ln -s etc/syslog-ng/conf.d/logstash.conf.disabled etc/syslog-ng/conf.d/logstash.conf  
+   ln -s /etc/syslog-ng/conf.d/logstash.conf.disabled etc/syslog-ng/conf.d/logstash.conf  
 fi
 
 /usr/sbin/syslog-ng --foreground
