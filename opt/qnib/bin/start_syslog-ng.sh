0aeae9e1e8d7c8ceda25a21d4180cb5d941b4736
Christian Kniep <christian.kniep@locafox.de>
2015-04-15 10:58:21 +0200

    explicitly enable SYSLOG forwarding

--- opt/qnib/bin/start_syslog-ng.sh
+++ opt/qnib/bin/start_syslog-ng.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "X${FORWARD_TO_LOGSTASH}" != "Xfalse" ];then
+if [ "X${FORWARD_TO_LOGSTASH}" == "Xtrue" -a ];then
    ln -sf /etc/syslog-ng/conf.d/logstash.conf.disabled /etc/syslog-ng/conf.d/logstash.conf  
 fi
 
