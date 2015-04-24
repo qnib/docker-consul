d4c246e725394711b4061afd6a120c3b5d16d904
Christian Kniep <christian@qnib.org>
2015-04-24 12:14:53 +0200

    typo in startscript of syslog-ng

--- opt/qnib/bin/start_syslog-ng.sh
+++ opt/qnib/bin/start_syslog-ng.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "X${FORWARD_TO_LOGSTASH}" == "Xtrue" -a ];then
+if [ "X${FORWARD_TO_LOGSTASH}" == "Xtrue" ];then
    ln -sf /etc/syslog-ng/conf.d/logstash.conf.disabled /etc/syslog-ng/conf.d/logstash.conf  
 fi
 
